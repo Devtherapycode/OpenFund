0a9b89e934689b5058487fa9f09d933574ef1be3
Vano Tsintsabadze <tsintsabadzevano@gmail.com>
2025-11-15 18:08:52 +0400

    feat: implement user registration validator and register validators

---
+++ OpenFund.API/Infrastructure/Validators/UserRegistrationValidator.cs
@@ -0,0 +1,26 @@
+using FluentValidation;
+using OpenFund.Core.DTOs;
+
+namespace OpenFund.API.Infrastructure.Validators;
+
+public class UserRegistrationValidator : AbstractValidator<UserRegistrationDto>
+{
+    public UserRegistrationValidator()
+    {
+        RuleFor(u => u.UserName)
+            .NotEmpty()
+            .MinimumLength(4)
+            .MaximumLength(50);
+
+        RuleFor(u => u.Email)
+            .NotEmpty()
+            .EmailAddress()
+            .MaximumLength(100);
+
+        RuleFor(u => u.Password)
+            .NotEmpty()
+            .MinimumLength(8)
+            .MaximumLength(50)
+            .Matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$\n");
+    }
+}
